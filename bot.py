6cce1c35b8bc4b25d11ce80a04d8227be79c8f16
CharmingMother <nawhpongmaji1@gmail.com>
2018-04-26 18:23:04 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,5 +11,15 @@ bot = commands.Bot(command_prefix="a.")
 
 @bot.event()
 async def on_ready():
+  print(bot.user.name)
 
+  
+  
+@bot.command()
+async def hi(ctx):
+  await bot.say("Hello there"+" "+ctx.message.author.name)
+  
+  
+  
+  
 bot.run(os.environ['BOT_TOKEN'])
